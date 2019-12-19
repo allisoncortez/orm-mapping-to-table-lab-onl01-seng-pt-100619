b883184939aaa1a12c54bbdcb13f8f1f5ab54269
allison cortez <allizetorc@gmail.com>
2019-12-19 17:10:05 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -24,4 +24,19 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+  
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+  
 end
